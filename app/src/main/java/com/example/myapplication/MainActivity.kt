2ddbec8ff43d46d99519af24d91a2f296f5eec07
BenjaminKadic <bkadic1@etf.unsa.ba>
2023-03-16 14:24:44 +0100

    pocetni commit na vjezbi2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,28 +3,25 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var listView: ListView
-    private lateinit var editText: EditText
-    private lateinit var button: Button
-    private val  listaVrijednosti = arrayListOf<String>()
-    private lateinit var adapter : MyArrayAdapter
+    private lateinit var favoriteMovies: RecyclerView
+    private lateinit var favoriteMoviesAdapter: MovieListAdapter
+    private var favoriteMoviesList =  getFavoriteMovies()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById(R.id.button1);
-        editText = findViewById(R.id.editText1)
-        listView = findViewById(R.id.listView1)
-        adapter = MyArrayAdapter(this, R.layout.element_list, listaVrijednosti)
-        listView.adapter=adapter
-        button.setOnClickListener {
-            addToList()
-        }
-    }
-    private fun addToList() {
-        listaVrijednosti.add(0,editText.text.toString())
-        adapter.notifyDataSetChanged();
-        editText.setText("");
+        favoriteMovies = findViewById(R.id.favoriteMovies)
+        favoriteMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        favoriteMoviesAdapter = MovieListAdapter(listOf())
+        favoriteMovies.adapter = favoriteMoviesAdapter
+        favoriteMoviesAdapter.updateMovies(favoriteMoviesList)
     }
 }
