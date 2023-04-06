301c0f636a760f70c994b661a3bd5e97e8068b7a
BenjaminKadic <bkadic1@etf.unsa.ba>
2023-04-06 14:03:46 +0200

    vjezba 5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,84 +1,31 @@
 package com.example.myapplication
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import android.net.ConnectivityManager
-import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var searchText: EditText
-    private lateinit var favoriteMovies: RecyclerView
-    private lateinit var favoriteMoviesAdapter: MovieListAdapter
-    private var favoriteMoviesList =  getFavoriteMovies()
-
-    private lateinit var recentMovies: RecyclerView
-    private lateinit var recentMoviesAdapter: MovieListAdapter
-    private var recentMoviesList =  getRecentMovies()
-
-    class NetworkChangeReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork = cm.activeNetworkInfo
-            val isConnected = activeNetwork?.isConnectedOrConnecting == true
-            if (!isConnected) {
-                val message = context.getString(R.string.offline)
-                Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        favoriteMovies = findViewById(R.id.favoriteMovies)
-        favoriteMovies.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        favoriteMoviesAdapter = MovieListAdapter(arrayListOf()) { movie -> showMovieDetails(movie) }
-        favoriteMovies.adapter = favoriteMoviesAdapter
-        favoriteMoviesAdapter.updateMovies(favoriteMoviesList)
-
-        recentMovies = findViewById(R.id.recentMovies)
-        recentMovies.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        recentMoviesAdapter = MovieListAdapter(arrayListOf()) { movie -> showMovieDetails(movie) }
-        recentMovies.adapter = recentMoviesAdapter
-        recentMoviesAdapter.updateMovies(recentMoviesList)
-
-        searchText = findViewById(R.id.searchText)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+        navView.setupWithNavController(navController)
 
         if(intent?.action == Intent.ACTION_SEND && intent?.type == "text/plain")
-            handleSendText(intent)
-
-
-        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-        registerReceiver(NetworkChangeReceiver(), filter)
-    }
-
-    private fun showMovieDetails(movie: Movie) {
-        val intent = Intent(this, MovieDetailActivity::class.java).apply {
-            putExtra("movie_title", movie.title)
+        {
+            intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+                val bundle = bundleOf("search" to it)
+                navView.selectedItemId= R.id.searchFragment
+                navController.navigate(R.id.searchFragment,bundle)
+            }
         }
-        startActivity(intent)
-    }
 
-    private fun handleSendText(intent: Intent) {
-        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-            searchText.setText(it)
-        }
     }
-
 }
