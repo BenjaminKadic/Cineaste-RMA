9950ad7c30ee39c636dc1f7b97ae5846c2d38d06
BenjaminKadic <bkadic1@etf.unsa.ba>
2023-05-18 13:42:59 +0200

    vjezba 8

--- app/src/main/java/com/example/myapplication/MovieRepository.kt
+++ app/src/main/java/com/example/myapplication/MovieRepository.kt
@@ -38,7 +38,7 @@ object MovieRepository {
                         val posterPath = movie.getString("poster_path")
                         val overview = movie.getString("overview")
                         val releaseDate = movie.getString("release_date")
-                        movies.add(Movie(id.toLong(), title, overview, releaseDate, null, null, posterPath, movie.getString("backdrop_path")))
+                        movies.add(Movie(id.toLong(), title, overview, releaseDate, null, posterPath, movie.getString("backdrop_path")))
                         if (i == 5) break
                     }
                 }
@@ -52,6 +52,14 @@ object MovieRepository {
                 return@withContext Result.Error(Exception("Cannot parse JSON"))
             }
 
+        }
+    }
+    suspend fun getUpcomingMovies(
+    ) : GetMoviesResponse?{
+        return withContext(Dispatchers.IO) {
+            var response = ApiAdapter.retrofit.getUpcomingMovies()
+            val responseBody = response.body()
+            return@withContext responseBody
         }
     }
 }
